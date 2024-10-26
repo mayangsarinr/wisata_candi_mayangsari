58cdab2a012718d4f4d2340096de76e68957217f
mayangsarinr <mayangsarinr_2327240064@mhs.mdp.ac.id>
2024-10-26 09:34:13 +0700

    add Profile Screen
    Menambahkan Profile Screen

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/screens/detail_screen.dart';
+import 'package:wisata_candi/screens/profile_screen.dart';
+
+void main() {
+  runApp(const MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wiisata Candi',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: ProfileScreen(),
+      // home: DetailScreen(candi: candiList[0], ),
+    );
+  }
+}
