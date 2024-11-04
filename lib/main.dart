55e48c844555035e31cc955c4a6dbab1ebf8d573
mayangsarinr <mayangsarinr_2327240064@mhs.mdp.ac.id>
2024-11-04 13:16:33 +0700

    SignInScreen
    aa

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/screens/SignInScreen.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
 import 'package:wisata_candi/screens/profile_screen.dart';
 
@@ -13,13 +14,26 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wiisata Candi',
+      title: 'Wisata Candi',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        appBarTheme: const AppBarTheme(
+          iconTheme:  IconThemeData(color: Colors.deepPurple),
+          titleTextStyle: TextStyle(
+            color:  Colors.deepPurple,
+            fontSize:  20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        colorScheme:
+        ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
+          primary: Colors.deepPurple,
+          surface: Colors.deepPurple[50],
+        ),
         useMaterial3: true,
       ),
-      home: ProfileScreen(),
+      //home: const ProfileScreen(),
       // home: DetailScreen(candi: candiList[0], ),
+      home: SignInscreen(),
     );
   }
 }
